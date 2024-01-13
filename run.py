3f663bbed942b4b2bbc0621e247cdbadb2f0f93a
kseen <korenevdn@gmail.com>
2024-01-13 20:23:14 +0300

    Refactor code to support sequential mode and add state saving

--- run.py
+++ run.py
@@ -1,29 +1,77 @@
-import random
+import secrets
 import aiohttp
 import asyncio
 import colorama
 import argparse
 import os
 import time
+import json
+import itertools
 
 o_filename = 'output.txt'
+state_filename = 'state.json'
 cloud_mail_mirror = 'https://cloud.mail.ru/public/'
 
 mcloud_alph = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-counter = 0
-success = 0
 speed_meter = 0
 speed = 0
 start_time = 0
 avg_speed = []
 avg_speed_sum = 0
+mode = 0  # 0 - random, 1 - sequential
+
+# to save:
+counter = 0
+success = 0
+seq_client = 0
+seq_success = 0
+
+
+def save_state():
+    global counter, success, seq_client, seq_success
+    state = {
+        'counter': counter,
+        'success': success,
+        'seq_client': seq_client,
+        'seq_success': seq_success,
+    }
+    with open(state_filename, 'w') as outfile:
+        json.dump(state, outfile)
+
+
+def load_state():
+    global counter, success, seq_client, seq_success
+    with open(state_filename) as json_file:
+        data = json.load(json_file)
+        counter = data['counter']
+        success = data['success']
+        seq_client = data['seq_client']
+        seq_success = data['seq_success']
 
 
 async def get_rnd_public_folder():
     global mcloud_alph
     return cloud_mail_mirror\
-        + ''.join(random.choice(mcloud_alph) for _ in range(4)) + '/' \
-        + ''.join(random.choice(mcloud_alph) for _ in range(9))
+        + ''.join(secrets.choice(mcloud_alph) for i in range(4)) + '/'\
+        + ''.join(secrets.choice(mcloud_alph) for i in range(9))\
+
+
+
+def get_combination(alphabet, number, length):
+    result = []
+    while number > 0:
+        number, i = divmod(number, len(alphabet))
+        result.append(alphabet[i])
+    return ''.join(result).rjust(length, alphabet[0])
+
+
+async def get_seq_public_folder():
+    global mcloud_alph, seq_client, cloud_mail_mirror
+    seq_client += 1
+    if seq_client > 473838133832161:
+        seq_client = 0
+    seq = get_combination(mcloud_alph, seq_client, 13)
+    return cloud_mail_mirror + seq[0:4] + '/' + seq[4:13]
 
 
 async def check_availability(url):
@@ -46,7 +94,14 @@ async def main(worker_id):
         avg_speed, avg_speed_sum
     while (1):
         try:
-            url = await get_rnd_public_folder()
+            if mode == 0:
+                url = await get_rnd_public_folder()
+            elif mode == 1:
+                url = await get_seq_public_folder()
+            else:
+                print('[' + colorama.Fore.RED + 'ERROR' + colorama.Fore.RESET
+                      + '] ' + 'Invalid mode')
+                exit()
             is_available = await check_availability(url)
             if is_available:
                 append_to_file(url)
@@ -68,15 +123,23 @@ async def main(worker_id):
             avg_speed_avg = round(avg_speed_avg, 1)
 
             print('[' + colorama.Fore.CYAN + 'INFO' + colorama.Fore.RESET + '] '
-                  + 'Checked: ' + colorama.Fore.RED + str(counter)
-                  + colorama.Fore.RESET + ' | Success: '
-                  + colorama.Fore.GREEN + str(success) + colorama.Fore.RESET
+                  + (('Checked: ' + colorama.Fore.RED + str(counter)
+                      + colorama.Fore.RESET)
+                      if mode == 0
+                     else ('Checked: ' + colorama.Fore.RED + str(seq_client)
+                           + colorama.Fore.RESET))
+                  + ((' | Success: '
+                      + colorama.Fore.GREEN + str(success)
+                      + colorama.Fore.RESET) if mode == 0
+                     else (' | Success: ' + colorama.Fore.GREEN
+                           + str(seq_success) + colorama.Fore.RESET))
                   + ' | URL: ' + colorama.Fore.LIGHTBLACK_EX + url
-                  + colorama.Fore.RESET + ' | Speed: '
+                  + colorama.Fore.RESET
+                  + ' | Speed: '
                   + colorama.Fore.LIGHTBLUE_EX + str(avg_speed_avg)
-                  + 'urls/s\t\t' + colorama.Fore.RESET, end='\r')
+                  + 'url/s\t\t' + colorama.Fore.RESET, end='\r')
         except Exception as e:
-            print('[' + colorama.Fore.RED + 'ERROR' +
+            print('[' + colorama.Fore.YELLOW + 'WARNING' +
                   colorama.Fore.RESET + '] ' + str(e) + ' at ' + url
                   + ', worker #' + str(worker_id))
 
@@ -103,33 +166,58 @@ async def try_gathered():
                     print('[' + colorama.Fore.RED + 'FAILED'
                           + colorama.Fore.RESET + '] ' + url, end='')
             except Exception as e:
-                print('[' + colorama.Fore.RED + 'ERROR' +
+                print('[' + colorama.Fore.YELLOW + 'WARNING' +
                       colorama.Fore.RESET + '] ' + str(e))
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    try:
+        parser = argparse.ArgumentParser()
 
-    parser.add_argument("-w", "--workers", help="Number of workers", type=int)
-    parser.add_argument(
-        "-c", "--check",
-        help="Check gathered links. May be provided with file using -o flag",
-        action="store_true")
-    parser.add_argument(
-        "-o", "--output", help="Output filename", type=str)
-    args = parser.parse_args()
+        parser.add_argument("-w", "--workers",
+                            help="Number of workers", type=int)
+        parser.add_argument(
+            "-c", "--check",
+            help="Check gathered links. May be provided with file using -o flag",
+            action="store_true")
+        parser.add_argument(
+            "-o", "--output", help="Output filename", type=str)
+        parser.add_argument(
+            "-s", "--sequential", help="Sequential mode", action="store_true")
+        args = parser.parse_args()
 
-    if args.output:
-        o_filename = args.output
+        if args.output:
+            o_filename = args.output
 
-    if args.workers:
-        num_workers = args.workers
-    else:
-        num_workers = 1  # Change this to the number of workers you want
+        if args.workers:
+            num_workers = args.workers
+        else:
+            num_workers = 1  # Change this to the number of workers you want
 
-    os.system('cls' if os.name == 'nt' else 'clear')
+        os.system('cls' if os.name == 'nt' else 'clear')
 
-    if args.check:
-        asyncio.run(try_gathered())
+        try:
+            load_state()
+        except:
+            print('[' + colorama.Fore.YELLOW + 'WARNING' +
+                  colorama.Fore.RESET + '] '
+                  + 'No state file found, starting from scratch...')
+
+        if args.check:
+            asyncio.run(try_gathered())
+            exit()
+
+        if args.sequential:
+            mode = 1
+
+        asyncio.run(run_all_workers(num_workers))
+
+    except KeyboardInterrupt:
+        save_state()
+        print('\n[' + colorama.Fore.YELLOW + 'WARNING' +
+              colorama.Fore.RESET + '] ' + 'KeyboardInterrupt, saving current state...')
         exit()
 
-    asyncio.run(run_all_workers(num_workers))
+    except Exception as e:
+        print('[' + colorama.Fore.RED + 'ERROR' + colorama.Fore.RESET + '] '
+              + str(e))
+        exit()
